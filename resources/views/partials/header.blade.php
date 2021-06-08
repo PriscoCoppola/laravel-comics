f59f61b1b3d4c351870cd47e064b2d652d5da4ac
PriscoCoppola <priscocoppola7@gmail.com>
2021-06-08 16:21:22 +0200

    add header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,9 +4,11 @@
             <img src="{{ asset('images/dc-logo.png') }}" alt="Logo">
         </a>
 
-        <ul class="container">
+        <ul>
             <li>
-                <a href="/">Comics</a>
+                <a class="active" href="/">Comics</a>
+            </li>
+            <li>
                 <a href="/news">News</a>
             </li>
         </ul>
