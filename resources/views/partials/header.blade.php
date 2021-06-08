d5434ad32d078bcd498cd091577072771f594687
PriscoCoppola <priscocoppola7@gmail.com>
2021-06-08 15:44:54 +0200

    add scss

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,14 @@
 <header>
-    header here
+    <div class="container">
+        <a href="/">
+            <img src="{{ asset('images/dc-logo.png') }}" alt="Logo">
+        </a>
+
+        <ul class="container">
+            <li>
+                <a href="/">Comics</a>
+                <a href="/news">News</a>
+            </li>
+        </ul>
+    </div>
 </header>
